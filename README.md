2ee79409418bd7729b4d70565941a057f2f709ea
Jonny-ar <rodriguez31793@gmail.com>
2018-01-05 18:01:14 -0800

    First commit in awhile. resolved the addrinfo issue. next commit will add cmd interface and more demonstratable test cases

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 THINGS TO DO
 
-Read/skim FTP protocol
+Write test cases for download/upload
+
+Implement command line interface taking in user I/O
+    to navigate directories and choose which/where to download/upload
+
+Later, command line I/O will be GUI based
+
+
+
--- testing_ground.py
+++ testing_ground.py
@@ -1,32 +1,32 @@
-from ftplib import FTP #import class FTP from ftplib
+from ftplib import FTP  # import class FTP from ftplib
 
+# connects directly to the host ip address
+# namespace -> ip address
+# default ftp port = 21
 
-#connects directly to the host ip address
-#namespace -> ip address
-#default ftp port = 21
-ftp = FTP('some ip address/domain name')
+ftp = FTP('speedtest.tele2.net')  # test server
 
 """
 unclear if FTP class initialization calls .connect
 if so, no need for ftp.connect(host,port)
 """
 
-ftp.login() #defaults to 'anonymous' for user,pw
+ftp.login()  # defaults to 'anonymous' for user,pw
 
-ftp.retrlines('LIST') # list directory content
+ftp.retrlines('LIST')  # list directory content
 
-#we could also list the files/directories to a file by calling
+# we could also list the files/directories to a file by calling
 files = ftp.dir()
-print ("Directory Contents: {}".format(files))
+print("Directory Contents: {}".format(files))
 
-#ftp.storlines() or ftp.storbinary() to upload to ftp server
+# ftp.storlines() or ftp.storbinary() to upload to ftp server
 
-ftp.cwd() #arg is path/directory to change into
+ftp.cwd('upload')  # arg is path/directory to change into
+ftp.retrlines('LIST') #what is in upload dir?
 
-ftp.quit() #finished
+ftp.quit()  # finished
 
-
-#FTP HOST SERVER
+# FTP HOST SERVER
 """
 Listening to port for incoming connects
 Have welcome msg ready
